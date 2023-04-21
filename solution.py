22a5f44504cdc7c6aafdf3741ab12d21da650130
Алексей Гришин <75268007+shlexsoid@users.noreply.github.com>
2023-04-22 00:04:30 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,7 +9,7 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-     res = proportions_ztest([x_success, y_success],
+    res = proportions_ztest([x_success, y_success],
                             [x_cnt, y_cnt],
                             alternative='larger')
     return res[1] < 0.08
